529d5adabf06daf72716872a02f04c2301319760
snowlight-aemt <it.s.aemt@gmail.com>
2023-03-01 20:29:06 +0900

    Add Partner Model

--- src/main/java/com/practice/order/domain/partner/Partner.java
+++ src/main/java/com/practice/order/domain/partner/Partner.java
@@ -1,19 +1,21 @@
 package com.practice.order.domain.partner;
 
+import com.practice.order.common.util.TokenGenerator;
+import com.practice.order.domain.AbstractEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.time.ZonedDateTime;
+import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 @Entity
 @NoArgsConstructor
 @Table(name = "partners")
-public class Partner {
+public class Partner extends AbstractEntity {
+    public static final String PREFIX_PARTNER = "ptn_";
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,22 +25,17 @@ public class Partner {
     private String partnerName;
     private String email;
 
-    private ZonedDateTime createdAt;
-    private ZonedDateTime updatedAt;
-
     @Builder
     public Partner(String businessNo, String partnerName, String email) {
-        if (businessNo == null) throw new RuntimeException("empty businessNo");
-        if (partnerName == null) throw new RuntimeException("empty partnerName");
-        if (email == null) throw new RuntimeException("empty email");
+        if (StringUtils.isEmpty(businessNo)) throw new RuntimeException("empty businessNo");
+        if (StringUtils.isEmpty(partnerName)) throw new RuntimeException("empty partnerName");
+        if (StringUtils.isEmpty(email)) throw new RuntimeException("empty email");
 
-        this.partnerToken = "ABCDE";
+        this.partnerToken = TokenGenerator.randomCharacterWithPrefix(PREFIX_PARTNER);
         this.businessNo = businessNo;
         this.partnerName = partnerName;
         this.email = email;
         this.status = Status.ENABLE;
-        this.createdAt = ZonedDateTime.now();
-        this.updatedAt = ZonedDateTime.now();
     }
 
     @Enumerated(EnumType.STRING)
@@ -51,11 +48,11 @@ public class Partner {
         private final String description;
     }
 
-    public void Enable() {
+    public void enable() {
         this.status = Status.ENABLE;
     }
 
-    public void Disable() {
+    public void disable() {
         this.status = Status.DISABLE;
     }
 }
