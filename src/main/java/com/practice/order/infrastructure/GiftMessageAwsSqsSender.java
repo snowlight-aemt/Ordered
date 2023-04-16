b93bbdba898b3cf4f96277ec612cc92c64f5b3b0
snowlight-aemt <it.s.aemt@gmail.com>
2023-04-16 13:35:57 +0900

    awspring sqs 라이브러리 변경

--- src/main/java/com/practice/order/infrastructure/GiftMessageAwsSqsSender.java
+++ src/main/java/com/practice/order/infrastructure/GiftMessageAwsSqsSender.java
@@ -1,34 +1,27 @@
 package com.practice.order.infrastructure;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.practice.order.domain.order.gift.GiftMessageChannelSender;
 import com.practice.order.domain.order.gift.GiftPaymentCompleteMessage;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.cloud.aws.messaging.core.SqsMessageHeaders;
 import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class GiftMessageAwsSqsSender implements GiftMessageChannelSender {
     private final String SQS_QUEUE_NAME = "order-payComplete.fifo";
-    private final QueueMessagingTemplate queueMessagingTemplate;
+    private final SqsTemplate sqsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void paymentComplete(GiftPaymentCompleteMessage message) {
         try {
-            Map<String, Object> headers = new HashMap<>();
-            headers.put(SqsMessageHeaders.SQS_GROUP_ID_HEADER, "item-queues");
-            headers.put(SqsMessageHeaders.SQS_DEDUPLICATION_ID_HEADER, UUID.randomUUID().toString());
-            queueMessagingTemplate.convertAndSend(SQS_QUEUE_NAME, message, headers);
-            log.info("[SQS enqueued topic: {}, message: {}", SQS_QUEUE_NAME, message);
-        } catch (Exception e) {
-            log.error("SQS failed topic: {}, message: {}", SQS_QUEUE_NAME, message);
+            sqsTemplate.send(SQS_QUEUE_NAME, objectMapper.writeValueAsString(message));
+        } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
